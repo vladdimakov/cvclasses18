90f6d63d30f92b8510391f13f9b3bdaa89c38c21
Vladislav Dimakov <dimakovv@gmail.com>
2018-10-09 20:42:15 +0300

    Added motion segmentation test

---
+++ cvlib/tests/motion_segmentation.cpp
@@ -0,0 +1,12 @@
+/* Split and merge segmentation algorithm testing.
+ * @file
+ * @date 2018-09-17
+ * @author Vladislav Dimakov
+ */
+
+#include <catch2/catch.hpp>
+
+#include "cvlib.hpp"
+
+using namespace cvlib;
+
