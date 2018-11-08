683d554aedd28d3cb87e23f24e74ffe685755575
Vladislav Dimakov <dimakovv@gmail.com>
2018-11-08 21:39:01 +0300

    Histogram showing was implemented

--- cvlib/src/corner_detector.cpp
+++ cvlib/src/corner_detector.cpp
@@ -48,31 +48,28 @@ void corner_detector_fast::detect(cv::InputArray image, CV_OUT std::vector<cv::K
         cv::cvtColor(image, m_image, cv::COLOR_BGR2GRAY);
 
     cv::Point2i point;
-
     for (point.y = 3; point.y < m_image.rows - 3; point.y++)
         for (point.x = 3; point.x < m_image.cols - 3; point.x++)
             if (isCorner(point, 4, 3) && isCorner(point, 1, 9))
-                keypoints.push_back(cv::KeyPoint(point, 1.f));
+                keypoints.push_back(cv::KeyPoint(point, 2, 0, 0, 0, 1));
+    // keypoints.push_back(cv::KeyPoint(point, 1.f));
 }
 
 void corner_detector_fast::compute(cv::InputArray, std::vector<cv::KeyPoint>& keypoints, cv::OutputArray descriptors)
 {
     std::srand(unsigned(std::time(0))); // \todo remove me
     // \todo implement any binary descriptor
-    const int desc_length = 2;
-    descriptors.create(static_cast<int>(keypoints.size()), desc_length, CV_32S);
-    auto desc_mat = descriptors.getMat();
-    desc_mat.setTo(0);
 
-    int* ptr = reinterpret_cast<int*>(desc_mat.ptr());
-    for (const auto& pt : keypoints)
+    cv::Mat a(keypoints.size(), 2, CV_8U);
+    for (int i = 0; i < keypoints.size(); i++)
     {
-        for (int i = 0; i < desc_length; ++i)
+        for (int j = 0; j < 2; j++)
         {
-            *ptr = std::rand();
-            ++ptr;
+            a.at<uchar>(i, j) = std::rand();
         }
     }
+
+    descriptors.assign(a);
 }
 
 void corner_detector_fast::detectAndCompute(cv::InputArray, cv::InputArray, std::vector<cv::KeyPoint>&, cv::OutputArray descriptors, bool /*= false*/)
