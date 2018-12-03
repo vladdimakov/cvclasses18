08690eb9b03eb4529f2bdcad014a4f16ee4cb150
Vladislav Dimakov <dimakovv@gmail.com>
2018-12-04 00:09:29 +0300

    Feature Matching was implemented

--- cvlib/src/descriptor_matcher.cpp
+++ cvlib/src/descriptor_matcher.cpp
@@ -13,29 +13,66 @@ void descriptor_matcher::set_ratio(float r)
     ratio_ = r;
 }
 
+void descriptor_matcher::erase_matches_by_ratio(std::vector<cv::DMatch> &matches)
+{
+	std::vector<bool> needToErase(matches.size());
+	for (int i = 1; i < matches.size(); i++)
+	{
+		if (matches[i - 1].distance / matches[i].distance <= ratio_)
+		{
+			needToErase[i - 1] = true;
+			needToErase[i] = true;
+		}
+	}
+
+	auto it = matches.begin();
+	int i = 0;
+	while (it != matches.end())
+	{
+		it = (needToErase[i]) ? matches.erase(it) : it + 1;
+		i++;
+	}
+}
+
 void descriptor_matcher::knnMatchImpl(cv::InputArray queryDescriptors, std::vector<std::vector<cv::DMatch>>& matches, int k /*unhandled*/,
                                       cv::InputArrayOfArrays masks /*unhandled*/, bool compactResult /*unhandled*/)
 {
-    if (trainDescCollection.empty())
-        return;
-
-    auto q_desc = queryDescriptors.getMat();
-    auto& t_desc = trainDescCollection[0];
-
-    matches.resize(q_desc.rows);
-
-    cv::RNG rnd;
-    for (int i = 0; i < q_desc.rows; ++i)
+    for (auto& curr_matches : matches)
     {
-        // \todo implement Ratio of SSD check.
-        matches[i].emplace_back(i, rnd.uniform(0, t_desc.rows), FLT_MAX);
+        if (curr_matches.size() > 1)
+        {
+            std::sort(curr_matches.begin(), curr_matches.end(),
+                      [](const cv::DMatch& obj1, const cv::DMatch& obj2) -> bool { return obj1.distance >= obj2.distance; });
+		
+			erase_matches_by_ratio(curr_matches);
+		}
     }
 }
 
 void descriptor_matcher::radiusMatchImpl(cv::InputArray queryDescriptors, std::vector<std::vector<cv::DMatch>>& matches, float maxDistance,
                                          cv::InputArrayOfArrays masks /*unhandled*/, bool compactResult /*unhandled*/)
 {
-    // \todo implement matching with "maxDistance"
+    if (trainDescCollection.empty())
+        return;
+
+    auto& ref_desc = trainDescCollection[0];
+    auto test_desc = queryDescriptors.getMat();
+
+    matches.resize(test_desc.rows);
+
+    double distance;
+    for (int i = 0; i < test_desc.rows; i++)
+    {
+        for (int j = 0; j < ref_desc.rows; j++)
+        {
+            distance = cv::norm(test_desc.row(i) - ref_desc.row(j), cv::NORM_L1);
+            if (distance <= maxDistance)
+            {
+                matches[i].emplace_back(i, j, distance);
+            }
+        }
+    }
+
     knnMatchImpl(queryDescriptors, matches, 1, masks, compactResult);
 }
 
